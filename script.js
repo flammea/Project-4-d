73c75506f0059a5a00e39472c54944c32cdd7cb7
Beata Markunas <beata.markunas@gmail.com>
2018-11-16 06:45:37 +0100

    Improve second addEventListener.

--- script.js
+++ script.js
@@ -5,5 +5,18 @@ div.style.left = `${divX}px`;
 div.style.top = `${divY}px`;
 
 div.addEventListener("mousedown", () => {
-    console.log("ok")
-})
+    console.log("mousedown");
+    div.style.backgroundColor = `grey`;
+});
+div.addEventListener("mousemove", (e) => {
+    console.log("mousemove");
+    divX = e.clientX;
+    divY = e.clientY;
+    div.style.left = `${divX}px`;
+    div.style.top = `${divY}px`;
+});
+div.addEventListener("mouseup", () => {
+    console.log("mouseup");
+    div.style.backgroundColor = `darkorange`;
+
+});
