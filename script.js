f64c43a2c77494821ef582458d44e00ad551936a
Beata Markunas <beata.markunas@gmail.com>
2018-11-16 06:54:52 +0100

    Create a flag variable.

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ let divY = 50;
 div.style.left = `${divX}px`;
 div.style.top = `${divY}px`;
 
+let moveActive = false;
+
 div.addEventListener("mousedown", () => {
     console.log("mousedown");
     div.style.backgroundColor = `grey`;
+    moveActive = !moveActive;
 });
 div.addEventListener("mousemove", (e) => {
     console.log("mousemove");
-    divX = e.clientX;
-    divY = e.clientY;
-    div.style.left = `${divX}px`;
-    div.style.top = `${divY}px`;
+    if (moveActive) {
+        divX = e.clientX;
+        divY = e.clientY;
+        div.style.left = `${divX -50}px`;
+        div.style.top = `${divY -50}px`;
+    }
 });
 div.addEventListener("mouseup", () => {
     console.log("mouseup");
     div.style.backgroundColor = `darkorange`;
-
+    moveActive = !moveActive;
 });
