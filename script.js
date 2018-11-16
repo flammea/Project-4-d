ae92d4c6ff68f5652c307628bf060ccabe2ec85a
Beata Markunas <beata.markunas@gmail.com>
2018-11-16 21:22:16 +0100

    Delete consol.logs

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ let insertDivX;
 let insertDivY;
 
 div.addEventListener("mousedown", (e) => {
-    console.log("mousedown");
     div.style.backgroundColor = `grey`;
     moveActive = !moveActive;
 
@@ -19,16 +18,14 @@ div.addEventListener("mousedown", (e) => {
     console.log(insertDivX, insertDivY)
 });
 div.addEventListener("mousemove", (e) => {
-    console.log("mousemove");
     if (moveActive) {
-        divX = e.clientX;
-        divY = e.clientY;
-        div.style.left = `${divX -50}px`;
-        div.style.top = `${divY -50}px`;
+        divX = e.clientX - insertDivX;
+        divY = e.clientY - insertDivY;
+        div.style.left = `${divX}px`;
+        div.style.top = `${divY}px`;
     }
 });
 div.addEventListener("mouseup", () => {
-    console.log("mouseup");
     div.style.backgroundColor = "darkorange";
     moveActive = !moveActive;
 });
